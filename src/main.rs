5051f197bf88a2d67e1ed4514c835c76aa6947a5
Danny <76977155+DNYFZR@users.noreply.github.com>
2025-03-13 09:49:45 +0000

    updated app layout

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 // Rust WASM Web App
 // #[allow(dead_code, unused_imports, unused_variables)]
-// mod utils;
 mod pages;
 mod components;
-use pages::{
-    home::Homepage, 
+use pages::home::Homepage;
+
+use components::{
     counter::Count,
+    warning::UnderConstruction
 };
-use components::warning::UnderConstruction;
 
 use leptos::prelude::*;
 use leptos_router::{components::*, path};
@@ -15,27 +15,37 @@ use leptos_router::{components::*, path};
 #[component]
 fn App() -> impl IntoView {
     view! {
-        <div class="app-main">
-            <h2>Corrosive</h2>
-   
-            <div>
-                <Router>
-                    <nav>
-                        <button><a href="/corrosive/">"Home"</a></button>
-                        <button><a href="/corrosive/counter">"Count"</a></button>
-                        <button><a href="/corrosive/about">"About"</a></button>
-                    </nav>
-                    <main>
-                        <Routes fallback=|| "Not found.">
-                            <Route path=path!("/corrosive/") view=Homepage />
-                            <Route path=path!("/corrosive/counter") view=Count />
-                            <Route path=path!("/corrosive/about") view=UnderConstruction />
-                            <Route path=path!("/*any") view=|| view! {<h2>"This is not the way..."</h2>} />
-                        </Routes>
-                    </main>
-                </Router>
+        <h2>Corrosive</h2> 
+        <AppRouter/>
+        <footer>
+            "🔥 🔥 🔥 Written in Rust & deployed with Web Assembly 🔥 🔥 🔥"
+        </footer>
+    }
+}
+
+#[component]
+fn AppRouter() -> impl IntoView {
+    view! {
+        <Router>
+            <div class="nav">
+                <nav>
+                    <button><a href="/corrosive/">"Home"</a></button>
+                    <button><a href="/corrosive/counter">"Count"</a></button>
+                    <button><a href="/corrosive/about">"About"</a></button>
+                </nav>
             </div>
-        </div>
+
+            <div class="app-main">
+                <main>
+                    <Routes fallback=|| "Not found.">
+                        <Route path=path!("/corrosive/") view=Homepage />
+                        <Route path=path!("/corrosive/counter") view=Count />
+                        <Route path=path!("/corrosive/about") view=UnderConstruction />
+                        <Route path=path!("/*any") view=|| view! {<h2>"This is not the way..."</h2>} />
+                    </Routes>
+                </main>
+            </div>
+        </Router>
     }
 }
 
